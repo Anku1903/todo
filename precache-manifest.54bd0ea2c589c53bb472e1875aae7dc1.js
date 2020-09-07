04e88ab729b55c065bf95b5394bf9323cdb510b4
Ankur1903 <ankurzalavadiya11@gmail.com>
2020-09-07 14:20:34 +0530

    Updates

---
+++ precache-manifest.54bd0ea2c589c53bb472e1875aae7dc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14fd30b5053f55abcd9b8d29a7c6f36",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "386d858f5185379020f1",
+    "url": "/todo/static/css/main.1f57dc66.chunk.css"
+  },
+  {
+    "revision": "eb9a1b13a31bd7c24555",
+    "url": "/todo/static/js/2.bdb3c31a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo/static/js/2.bdb3c31a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "386d858f5185379020f1",
+    "url": "/todo/static/js/main.60c193ad.chunk.js"
+  },
+  {
+    "revision": "f691e6deaea99be16b10",
+    "url": "/todo/static/js/runtime-main.af28369c.js"
+  }
+]);
